699cddfa040630830f9ac04a6a2241ab0de7ad90
haryi <haryi@HARUNPC>
2022-08-23 22:54:14 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            Car car1 = new Car { BrandId = 7, ColorId = 3, DailyPrice = 150235, Description = "haki turkuaz", Id = 8, ModelYear = 1993 };
+
+            carManager.Add(car1);
+            foreach (var car in carManager.GetAll())
+            {
+                if (car.Description == "haki turkuaz")
+                {
+                    Console.WriteLine(car.DailyPrice);
+                }
+            }
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            carManager.Delete(car1);
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+           
+
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> { new Car { Id = 1, BrandId = 1 , ColorId = 1 , DailyPrice = 10000, ModelYear = 2005, Description = "mavi"},
+                                    new Car { Id = 2, BrandId = 2 , ColorId = 12 , DailyPrice = 250000, ModelYear = 2022, Description = "sarı"},
+                                    new Car { Id = 3, BrandId = 3 , ColorId = 3 , DailyPrice = 100000, ModelYear = 2008, Description = "kırmızı"},
+                                    new Car { Id = 4, BrandId = 4 , ColorId = 4 , DailyPrice = 75000, ModelYear = 2011, Description = "lacivert"},
+                                    new Car { Id = 5, BrandId = 5 , ColorId = 5 , DailyPrice = 30000, ModelYear = 2007, Description = "yeşil"},
+                                   };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+            
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+            carToUpdate.DailyPrice = car.DailyPrice;
+
+        }
+    }
+}
